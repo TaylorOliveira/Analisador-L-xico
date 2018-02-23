47abd7dff4f4c68ccb5db7e3c515126d21638761
Taylor <taylorsantosoliveira@hotmail.com>
2018-02-23 14:53:31 -0300

    organização

--- programa.py
+++ programa.py
@@ -43,86 +43,92 @@ class AnalisadorLexico():
                 return self.reservadas[i]
             i+=1
 
-    def averiguar(self):
+    def arquivo(self):
         arq_saida = open(self.arquivo_saida,'w')
         if not os.path.exists(self.arquivo_entrada):
             arq_saida.write("Arquivo de entrada inexistente")
-            return
-        arq_entrada = open(self.arquivo_entrada, 'r')
-
-        linha_codigo = arq_entrada.readline()
-        numero_linha = 0
-
+            # print("Open")
+            return        
         cabecalho = "IDENTIFICAÇAO;TOKEN;POSICAO LINHA; POSICAO COLUNA\n"
         arq_saida.write(cabecalho)
+        return arq_saida
 
-        while linha_codigo:
-            i = 0
-            tamanho_linha = len(linha_codigo)
-            while i < tamanho_linha:
-                carac_atual = linha_codigo[i]
-                print(i)
-                print(carac_atual)
-                carac_seguinte = None
-                if (i+1) < tamanho_linha:
-                    carac_seguinte = linha_codigo[i+1]
-                if carac_seguinte != None and self.averiguar_operadores(carac_atual+carac_seguinte):
-                    arq_saida.write('Operador;'+carac_atual+carac_seguinte+';'+str(numero_linha)+';'+str(i)+'\n')
-                    i+=1
-                elif self.averiguar_operadores(carac_atual):
-                    arq_saida.write('Operador;'+carac_atual+';'+str(numero_linha)+';'+str(i)+'\n')
-                    #print(i)
-                elif self.averiguar_digito(carac_atual):
-                    string_temp = carac_atual
-                    #print(carac_atual)
-                    i += 1
+    def averiguar(self):        
+        arq_saida = self.arquivo()
+        if not arq_saida:
+            pass
+        else:
+            arq_entrada = open(self.arquivo_entrada, 'r')
+            linha_codigo = arq_entrada.readline()        
+            numero_linha = 0
+            
+            while linha_codigo:
+                i = 0
+                tamanho_linha = len(linha_codigo)
+                while i < tamanho_linha:
                     carac_atual = linha_codigo[i]
-                    while self.averiguar_digito(carac_atual) and (i+1 < tamanho_linha):
-                        string_temp += carac_atual
-                        #print(string_temp)
-                        i+=1                        
-                        carac_atual = linha_codigo[i]                    
-                    arq_saida.write('Número inteiro;'+string_temp+';'+str(numero_linha)+';'+str(i)+'\n')
-                    if not self.averiguar_digito(carac_atual):
-                       i -= 1
-                elif self.averiguar_delimitador(carac_atual):
-                    arq_saida.write('Delimitador;'+ carac_atual +';'+str(numero_linha)+';'+str(i)+'\n')
-                elif self.averiguar_letra(carac_atual):                    
-                    string_temp = carac_atual
-                    i+=1
-                    algum_erro = False
-                    while i < tamanho_linha:                        
-                        carac_seguinte = None
+                    print(i)
+                    print(carac_atual)
+                    carac_seguinte = None
+                    if (i+1) < tamanho_linha:
+                        carac_seguinte = linha_codigo[i+1]
+                    if carac_seguinte != None and self.averiguar_operadores(carac_atual+carac_seguinte):
+                        arq_saida.write('Operador;'+carac_atual+carac_seguinte+';'+str(numero_linha)+';'+str(i)+'\n')
+                        i+=1
+                    elif self.averiguar_operadores(carac_atual):
+                        arq_saida.write('Operador;'+carac_atual+';'+str(numero_linha)+';'+str(i)+'\n')
+                        #print(i)
+                    elif self.averiguar_digito(carac_atual):
+                        string_temp = carac_atual
+                        #print(carac_atual)
+                        i += 1
                         carac_atual = linha_codigo[i]
-                        if i+1 < tamanho_linha:
-                            cara_seguinte = linha_codigo[i+1]
-                        if self.averiguar_letra(carac_atual) or self.averiguar_digito(carac_atual) or carac_atual == '_':
+                        while self.averiguar_digito(carac_atual) and (i+1 < tamanho_linha):
                             string_temp += carac_atual
                             #print(string_temp)
-                        elif self.averiguar_delimitador(carac_atual) or carac_atual == ' ' or carac_atual == '\t' or carac_atual == '\r':
-                            i -= 1
-                            break
-                        elif carac_seguinte != None and self.averiguar_operadores(carac_atual+carac_seguinte) or self.averiguar_operadores(carac_atual):
-                            i-=1
-                            break
-                        elif carac_atual != '\n':
-                            algum_erro = True
-                            break                            
+                            i+=1                        
+                            carac_atual = linha_codigo[i]                    
+                        arq_saida.write('Número inteiro;'+string_temp+';'+str(numero_linha)+';'+str(i)+'\n')
+                        if not self.averiguar_digito(carac_atual):
+                           i -= 1
+                    elif self.averiguar_delimitador(carac_atual):
+                        arq_saida.write('Delimitador;'+ carac_atual +';'+str(numero_linha)+';'+str(i)+'\n')
+                    elif self.averiguar_letra(carac_atual):                    
+                        string_temp = carac_atual
                         i+=1
-                    if algum_erro:
-                        while i+1 < tamanho_linha:
-                            i+=1
+                        algum_erro = False
+                        while i < tamanho_linha:                        
+                            carac_seguinte = None
                             carac_atual = linha_codigo[i]
-                            if self.averiguar_delimitador(carac_atual) or carac_atual == ' ' or carac_atual == '\t' or carac_atual == '\r':
+                            if i+1 < tamanho_linha:
+                                cara_seguinte = linha_codigo[i+1]
+                            if self.averiguar_letra(carac_atual) or self.averiguar_digito(carac_atual) or carac_atual == '_':
+                                string_temp += carac_atual
+                                #print(string_temp)
+                            elif self.averiguar_delimitador(carac_atual) or carac_atual == ' ' or carac_atual == '\t' or carac_atual == '\r':
+                                i -= 1
+                                break
+                            elif carac_seguinte != None and self.averiguar_operadores(carac_atual+carac_seguinte) or self.averiguar_operadores(carac_atual):
                                 i-=1
                                 break
-                    else:
-                        if self.tokens_reservados(string_temp):
-                            arq_saida.write('Token Reservado;'+self.qual_token_reservado(string_temp)+';'+str(numero_linha)+';'+str(i)+'\n')
+                            elif carac_atual != '\n':
+                                algum_erro = True
+                                break                            
+                            i+=1
+                        if algum_erro:
+                            while i+1 < tamanho_linha:
+                                i+=1
+                                carac_atual = linha_codigo[i]
+                                if self.averiguar_delimitador(carac_atual) or carac_atual == ' ' or carac_atual == '\t' or carac_atual == '\r':
+                                    i-=1
+                                    break
                         else:
-                            arq_saida.write('Identificador;'+string_temp+';'+str(numero_linha)+';'+str(i)+'\n')
-                i+=1
-            linha_codigo = arq_entrada.readline()
-            numero_linha += 1
-        arq_entrada.close()
-        arq_saida.close()
+                            if self.tokens_reservados(string_temp):
+                                arq_saida.write('Token Reservado;'+self.qual_token_reservado(string_temp)+';'+str(numero_linha)+';'+str(i)+'\n')
+                            else:
+                                arq_saida.write('Constante;'+string_temp+';'+str(numero_linha)+';'+str(i)+'\n')
+                    i+=1
+                linha_codigo = arq_entrada.readline()
+                numero_linha += 1
+            arq_entrada.close()
+            arq_saida.close()
